2e64854247bab973132ba5a08cb72d4d73193956
luellancarvalho <67391971+luellancarvalho@users.noreply.github.com>
2020-09-14 14:43:09 -0600

    Add server side validation.

--- models/user.js
+++ models/user.js
@@ -7,20 +7,24 @@ var userSchema = new Schema({
        type: String,
        lowercase: true,
        trim: true,
-       required: true,
+       required: [true, 'Username is required.'],
+       match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Username must be a valid email address']
    },
-   password: { type: String, required: true } 
+   password: {
+       type: String,
+       required: [true, 'Password is required.']
+   }
 },
 {
    timestamps: true
 });
 
 userSchema.pre("save", function(next) {
-   if(!this.isModified("password")) {
-       return next();
-   }
-   this.password = Bcrypt.hashSync(this.password, 10);
-   next();
+    if(!this.isModified("password")) {
+        return next();
+    }
+    this.password = Bcrypt.hashSync(this.password, 10);
+    next();
 });
 
 var User = mongoose.model('User', userSchema);
