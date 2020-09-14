22e2c256e621477daff61c8faf03588bf25197cf
luellancarvalho <67391971+luellancarvalho@users.noreply.github.com>
2020-09-14 13:48:16 -0600

    Add update user Schema.

--- models/user.js
+++ models/user.js
@@ -1,9 +1,26 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var Bcrypt = require("bcryptjs");
 
 var userSchema = new Schema({
-   username: { type: String, required: true },
+   username: {
+       type: String,
+       lowercase: true,
+       trim: true,
+       required: true,
+   },
    password: { type: String, required: true } 
+},
+{
+   timestamps: true
+});
+
+userSchema.pre("save", function(next) {
+   if(!this.isModified("password")) {
+       return next();
+   }
+   this.password = Bcrypt.hashSync(this.password, 10);
+   next();
 });
 
 var User = mongoose.model('User', userSchema);
